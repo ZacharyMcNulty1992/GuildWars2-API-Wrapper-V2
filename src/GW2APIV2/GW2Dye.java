46e86c55700cb09f57b13da22069755fb0af03f4
Zachary McNulty <zach.mcnulty1992@gmail.com>
2015-09-05 16:45:58 -0400

    Finished the Constructor, and reduced the number of variables

--- src/GW2APIV2/GW2Dye.java
+++ src/GW2APIV2/GW2Dye.java
@@ -1,5 +1,6 @@
 package GW2APIV2;
 
+import java.util.HashMap;
 import java.util.List;
 
 import org.json.simple.JSONObject;
@@ -11,31 +12,52 @@ public class GW2Dye {
 	public List<Long> BaseRGBValues;
 	
 	//when applied to cloth
-	public Long clothBrightness;
-	public Long clothContrast;
-	public Long clothHue;
-	public Long clothSaturation;
-	public Long clothLightness;
+	public HashMap<String, Long> Cloth;
 	public List<Long> clothRGB;
 	
 	//when applied to leather
-	public Long leatherBrightness;
-	public Long leatherContrast;
-	public Long leatherHue;
-	public Long leatherSaturation;
-	public Long leatherLightness;
+	public HashMap<String, Long> Leather;
 	public List<Long> leatherRGB;
 	
 	//when applied to metal
-	public Long metalBrightness;
-	public Long metalContrast;
-	public Long metalHue;
-	public Long metalSaturation;
-	public Long metalLightness;
+	public HashMap<String, Long> Metal;
 	public List<Long> metalRGB;
 	
 	public GW2Dye(JSONObject a){
 		
+		id = (Long) a.get("id");
+		name = (String) a.get("name");
+		BaseRGBValues = (List) a.get("base_rgb");
+		
+		//get cloth values
+		JSONObject obj = (JSONObject) a.get("cloth");
+		
+		Cloth.put("Brightness", (Long) obj.get("brightness"));
+		Cloth.put("Contrast", (Long) obj.get("contrast"));
+		Cloth.put("Hue", (Long) obj.get("hue"));
+		Cloth.put("Saturation", (Long) obj.get("stauration"));
+		Cloth.put("Lightness", (Long) obj.get("lightness"));
+		clothRGB = (List) obj.get("rgb");
+		
+		//leather values
+		obj = (JSONObject) a.get("leather");
+		
+		Leather.put("Brightness", (Long) obj.get("brightness"));
+		Leather.put("Contrast", (Long) obj.get("contrast"));
+		Leather.put("Hue", (Long) obj.get("hue"));
+		Leather.put("Saturation", (Long) obj.get("stauration"));
+		Leather.put("Lightness", (Long) obj.get("lightness"));
+		leatherRGB = (List) obj.get("rgb");
+		
+		//metal values
+		obj  = (JSONObject) a.get("metal");
+		
+		Metal.put("Brightness", (Long) obj.get("brightness"));
+		Metal.put("Contrast", (Long) obj.get("contrast"));
+		Metal.put("Hue", (Long) obj.get("hue"));
+		Metal.put("Saturation", (Long) obj.get("stauration"));
+		Metal.put("Lightness", (Long) obj.get("lightness"));
+		metalRGB = (List) obj.get("rgb");
 	}
 	
 }
