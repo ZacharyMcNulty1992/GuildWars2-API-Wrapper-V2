98e010ec4b909f09dd88ad2ee83ccee77463515c
Zachary McNulty <zach.mcnulty1992@gmail.com>
2015-09-09 15:57:22 -0400

    created this object to hold more specific information about traits

---
+++ src/GW2APIV2/Account/GW2TraitedFacts.java
@@ -0,0 +1,25 @@
+package GW2APIV2.Account;
+
+import org.json.simple.JSONObject;
+
+public class GW2TraitedFacts {
+	
+	public String text;
+	public String iconURL;
+	public String type;
+	
+	//only have values if the object is a Traited Facts object, else these will be null
+	public Long requiresTrait;
+	public Long overrides;
+	
+	public GW2TraitedFacts(JSONObject o){
+		
+		text = (String) o.get("text");
+		iconURL = (String) o.get("icon");
+		type = (String) o.get("type");
+		
+		requiresTrait = (Long) o.get("requires_trait");
+		overrides = (Long) o.get("overrides");
+	}
+	
+}
