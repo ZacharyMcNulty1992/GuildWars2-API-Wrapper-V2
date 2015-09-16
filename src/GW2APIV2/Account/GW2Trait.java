e1c4f09eccb58eac805327bcdb9cc6a587d1cd7a
Zachary McNulty <zach.mcnulty1992@gmail.com>
2015-09-15 21:38:08 -0400

    TraitedFacts and Facts List now have the appropriate subclasses added

--- src/GW2APIV2/Account/GW2Trait.java
+++ src/GW2APIV2/Account/GW2Trait.java
@@ -4,25 +4,32 @@ import java.util.List;
 
 import org.json.simple.JSONObject;
 
-import GW2APIV2.Account.Trait.GW2TraitSkill;
-import GW2APIV2.Account.Trait.GW2TraitedFacts;
+import GW2APIV2.Account.Trait.*;
 
 public class GW2Trait {
 	
-	public Long id; //id of the trait
-	public String name; //name of the trait
-	public String description; //description of the trait
-	public Long spec_id; //id of the specialization this trait belongs to
-	public Long tier; //tier of the trait (ie: Adept, Master, Grandmaster) as a value between 1-3
-	public String slot; //Either major or minor, minor traits are the ones given immediately upon chooseing a specialization
+	private Long id; //id of the trait
+	private String name; //name of the trait
+	private String description; //description of the trait
+	private Long spec_id; //id of the specialization this trait belongs to
+	private Long tier; //tier of the trait (ie: Adept, Master, Grandmaster) as a value between 1-3
+	private String slot; //Either major or minor, minor traits are the ones given immediately upon chooseing a specialization
 	
-	private List<GW2TraitedFacts> traitedFacts;
-	private List<GW2TraitedFacts> facts;
+	private List<GW2TraitedFacts> traitedFacts; //a list of additions or changes to tooltip facts
+	private List<GW2TraitedFacts> facts; //a lits of tooltips that belong to the skill
 	
-	public List<GW2TraitSkill> skill; // list of skills triggered by this trait / may be null
+	private List<GW2TraitSkill> skill; // list of skills triggered by this trait / may be null
+	
+	
+	/*
+	 * Constructor
+	 * Params: a JSONObject that contains a trait
+	 * Throws: a Runtime Exception if the Facts or the TraitedFacts sub-objects contain invalid types
+	 */
 	
 	public GW2Trait(JSONObject o){
 		
+		//Initialize variables
 		id = (Long) o.get("id");
 		name = (String) o.get("name");
 		description = (String) o.get("description");
@@ -33,13 +40,90 @@ public class GW2Trait {
 		List<JSONObject> TF = (List) o.get("traited_fects");
 		List<JSONObject> F = (List) o.get("facts");
 		
+		String type = "";
+		
 		//change these to the appropriate sub class type
 		for(JSONObject a : TF){
-			traitedFacts.add(new GW2TraitedFacts(a));
+			
+			type = (String) a.get("type");
+			
+			
+			//this switch is for each different subclass of the traitedFacts object
+			//if a type is not found this will throw a runtime exception
+			switch(type){
+				case "AttributeAdjust" : traitedFacts.add(new GW2AttributeAdjust(a));
+										 break;
+				case "Buff" : traitedFacts.add(new GW2Buff(a));
+							  break;
+				case "BuffConversion" : traitedFacts.add(new GW2BuffConversion(a));
+										break;
+				case "ComboField" : traitedFacts.add(new GW2ComboField(a));
+									break;
+				case "ComboFinisher" : traitedFacts.add(new GW2ComboFinisher(a));
+									   break;
+				case "Damage" : traitedFacts.add(new GW2Damage(a));
+								break;
+				case "Distance" : traitedFacts.add(new GW2Distance(a));
+								  break;
+				case "noData" : traitedFacts.add(new GW2TraitedFacts(a));
+								break;
+				case "Number" : traitedFacts.add(new GW2Number(a));
+								break;
+				case "Percent" : traitedFacts.add(new GW2Percent(a));
+								 break;
+				case "PrefixedBuff" : traitedFacts.add(new GW2PrefixedBuff(a));
+									  break;
+				case "Radius" : traitedFacts.add(new GW2Radius(a));
+								break;
+				case "Range" : traitedFacts.add(new GW2Range(a));
+								break;
+				case "Recharge" : traitedFacts.add(new GW2Recharge(a));
+								  break;
+				case "Time" : traitedFacts.add(new GW2Time(a));
+							  break;
+				case "Unblockable" : traitedFacts.add(new GW2Unblockable(a));
+									 break;
+				default : throw new RuntimeException();
+			}
 		}
 		
 		for(JSONObject a : F){
-			facts.add(new GW2TraitedFacts(a));
+			
+			switch(type){
+				case "AttributeAdjust" : facts.add(new GW2AttributeAdjust(a));
+										  break;
+				case "Buff" : facts.add(new GW2Buff(a));
+						      break;
+				case "BuffConversion" : facts.add(new GW2BuffConversion(a));
+									    break;
+				case "ComboField" : facts.add(new GW2ComboField(a));
+								    break;
+				case "ComboFinisher" : facts.add(new GW2ComboFinisher(a));
+								       break;
+				case "Damage" : facts.add(new GW2Damage(a));
+							    break;
+				case "Distance" : facts.add(new GW2Distance(a));
+							      break;
+				case "noData" : facts.add(new GW2TraitedFacts(a));
+							    break;
+				case "Number" : facts.add(new GW2Number(a));
+							    break;
+				case "Percent" : facts.add(new GW2Percent(a));
+							     break;
+				case "PrefixedBuff" : facts.add(new GW2PrefixedBuff(a));
+								      break;
+				case "Radius" : facts.add(new GW2Radius(a));
+							    break;
+				case "Range" : facts.add(new GW2Range(a));
+							   break;
+				case "Recharge" : facts.add(new GW2Recharge(a));
+							      break;
+				case "Time" : facts.add(new GW2Time(a));
+							  break;
+				case "Unblockable" : facts.add(new GW2Unblockable(a));
+								     break;
+				default : throw new RuntimeException();
+			}
 		}
 	}
 }
