a74ace29e6cb38b777fb303e023c319a1529ca2c
zacharymcnulty <zach.mcnulty1992@gmail.com>
2015-10-06 12:10:47 -0400

    added comments and added getters as well as made most varibales private

--- src/GW2APIV2/Account/GW2Character.java
+++ src/GW2APIV2/Account/GW2Character.java
@@ -12,27 +12,36 @@ import GW2APIV2.Items.GW2Item;
 
 public class GW2Character {
 	
-	public String name;
-	public String race;
-	public String gender;
-	public String profession; //the class of the character ie: Elementalist
-	public Long level;
-	public String guild;//guilds this character is apart of
-	public String creation_time;// date and time of creation of character
-	public Long age; // number of seconds a character has been played
-	public Long deaths;
-	public List<GW2Item> bags; //items in characters bags
-	public List<GW2Item> equips; //items currently equipped on character
+	private String name; //name of the character
+	private String race; //the race of the character
+	private String gender; //male or female
+	private String profession; //the class of the character ie: Elementalist
+	private Long level; //level of the character
+	private String guild;//guilds this character is apart of
+	private String creation_time;// date and time of creation of character
+	private Long age; // number of seconds a character has been played
+	private Long deaths; //number of deaths on this character
+	private List<GW2Item> bags; //items in characters bags
+	private List<GW2Item> equips; //items currently equipped on character
 	
-	protected InternetConnection ic;
-	protected String standardURL;
-	protected boolean bp;
-	
-	
+	protected InternetConnection ic; //internet connection class for obtaining character information from character endpoints
+	protected String standardURL; //url for character endpoints
+	protected boolean bp; //bag permissions
+	protected boolean buildPermission;
+	/*
+	 * Constructor
+	 * 
+	 * Parameters:
+	 * 		String name: the name of the character
+	 * 		String apiKey: the apiKey associated with the account
+	 * 		boolean bagPermission: bag permission of the supplied api-key
+	 * 		boolean build: the build permission of the supplied api-key
+	 */
 	public GW2Character(String name, String apiKey, boolean bagPermission, boolean build){
 		this.name = name;
 		standardURL = "https://api.guildwars2.com/v2/characters";
 		bp = bagPermission;
+		buildPermission = build;
 		
 		try{
 			//gets the JSONObject of this character
@@ -51,6 +60,15 @@ public class GW2Character {
 		
 	}
 	
+	/*
+	 * parseObject
+	 * 
+	 * Parameters:
+	 * 		JSONObject a: a JSONObject from the v2/characters/{character name} endpoint
+	 * 
+	 * Function:
+	 * 		parses all the information within the JSONObject 
+	 */
 	protected void parseObject(JSONObject a){
 		
 		race = (String) a.get("race");
@@ -62,10 +80,26 @@ public class GW2Character {
 		creation_time = (String) a.get("created");
 		deaths = (Long) a.get("deaths");
 		
+		//check to see what permissions are available
 		if(bp){
 			invInfo((JSONArray)a.get("equipment"), (JSONArray)a.get("bags"));
 		}	
+		if(buildPermission){
+			
+		}
+		
 	}
+	
+	/*
+	 * invInfo
+	 * 
+	 * Parameters:
+	 * 		JSONArray eq: an array of JSONObjects that represent items currently equipted to the character
+	 * 		JSONArray bags: an array of JSONObjects that represent items currently in a characters bags or inventory
+	 * 
+	 * Function:
+	 * 		adds items to lists that will represent the items currently equipted, and items currently in a characters inventory or bags 
+	 */
 	
 	protected void invInfo(JSONArray eq, JSONArray bags){
 		
@@ -98,4 +132,52 @@ public class GW2Character {
 		
 	}
 	
+	/***********
+	 * Getters *
+	 ***********/
+	public String getName(){
+		return name;
+	}
+	
+	public String getRace(){
+		return race;
+	}
+	
+	public String getGender(){
+		return gender;
+	}
+	
+	public String getProfession(){
+		return profession;
+	}
+	
+	public Long getLevel(){
+		return level;
+	}
+	
+	public String getGuild(){
+		return guild;
+	}
+	
+	public String getCreationTime(){
+		return creation_time;
+	}
+	
+	public Long getAge(){
+		return age;
+	}
+	
+	public Long getDeaths(){
+		return deaths;
+	}
+	
+	public List<GW2Item> getBags(){
+		return bags;
+	}
+	
+	public List<GW2Item> getEquips(){
+		return equips;
+	}
 }
+
+
