689f1a531e23b8e7b668efb66c411a9ac5808f45
zacharymcnulty <zach.mcnulty1992@gmail.com>
2015-10-18 13:11:48 -0400

    fixed an issue where values were being incorrectly parsed

--- src/GW2APIV2/Items/GW2Dye.java
+++ src/GW2APIV2/Items/GW2Dye.java
@@ -1,8 +1,9 @@
 package GW2APIV2.Items;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-
+import java.lang.Number;
 import org.json.simple.JSONObject;
 
 public class GW2Dye {
@@ -12,15 +13,15 @@ public class GW2Dye {
 	private List<Long> BaseRGBValues;
 	
 	//when applied to cloth
-	private HashMap<String, Long> Cloth;
+	private HashMap<String, Number> Cloth;
 	private List<Long> clothRGB;
 	
 	//when applied to leather
-	private HashMap<String, Long> Leather;
+	private HashMap<String, Number> Leather;
 	private List<Long> leatherRGB;
 	
 	//when applied to metal
-	private HashMap<String, Long> Metal;
+	private HashMap<String, Number> Metal;
 	private List<Long> metalRGB;
 	
 	public GW2Dye(JSONObject a){
@@ -29,40 +30,67 @@ public class GW2Dye {
 		name = (String) a.get("name");
 		BaseRGBValues = (List) a.get("base_rgb");
 		
+		Cloth = new HashMap<String, Number>();
+		Leather = new HashMap<String, Number>();
+		Metal = new HashMap<String, Number>();
+		
 		//get cloth values
 		JSONObject obj = (JSONObject) a.get("cloth");
 		
-		Cloth.put("Brightness", (Long) obj.get("brightness"));
-		Cloth.put("Contrast", (Long) obj.get("contrast"));
-		Cloth.put("Hue", (Long) obj.get("hue"));
-		Cloth.put("Saturation", (Long) obj.get("stauration"));
-		Cloth.put("Lightness", (Long) obj.get("lightness"));
+		
+		Cloth.put("Brightness", (Number) obj.get("brightness"));
+		Cloth.put("Contrast", (Number)obj.get("contrast"));
+		Cloth.put("Hue", (Number)obj.get("hue"));
+		Cloth.put("Saturation", (Number) obj.get("saturation"));
+		Cloth.put("Lightness", (Number) obj.get("lightness"));
 		clothRGB = (List) obj.get("rgb");
 		
 		//leather values
 		obj = (JSONObject) a.get("leather");
 		
-		Leather.put("Brightness", (Long) obj.get("brightness"));
-		Leather.put("Contrast", (Long) obj.get("contrast"));
-		Leather.put("Hue", (Long) obj.get("hue"));
-		Leather.put("Saturation", (Long) obj.get("stauration"));
-		Leather.put("Lightness", (Long) obj.get("lightness"));
+		Leather.put("Brightness", (Number) obj.get("brightness"));
+		Leather.put("Contrast", (Number) obj.get("contrast"));
+		Leather.put("Hue", (Number) obj.get("hue"));
+		Leather.put("Saturation", (Number) obj.get("saturation"));
+		Leather.put("Lightness", (Number) obj.get("lightness"));
 		leatherRGB = (List) obj.get("rgb");
 		
 		//metal values
 		obj  = (JSONObject) a.get("metal");
 		
-		Metal.put("Brightness", (Long) obj.get("brightness"));
-		Metal.put("Contrast", (Long) obj.get("contrast"));
-		Metal.put("Hue", (Long) obj.get("hue"));
-		Metal.put("Saturation", (Long) obj.get("stauration"));
-		Metal.put("Lightness", (Long) obj.get("lightness"));
+		Metal.put("Brightness", (Number) obj.get("brightness"));
+		Metal.put("Contrast", (Number) obj.get("contrast"));
+		Metal.put("Hue", (Number) obj.get("hue"));
+		Metal.put("Saturation", (Number) obj.get("saturation"));
+		Metal.put("Lightness", (Number) obj.get("lightness"));
 		metalRGB = (List) obj.get("rgb");
 	}
+	
+	private Long turnThisDamnObjIntoLong(JSONObject o, String key){
+		
+		Number x = 0;
+		Long v = null;
+		
+		try{
+			x = (Number) o.get(key);
+			v = new Long((int)x);
+		}
+		catch(ClassCastException f){
+			
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+	
 	
 	/***********
 	 * Getters *
 	 ***********/
+	
+	
 	public Long getId(){
 		return id;
 	}
@@ -75,7 +103,7 @@ public class GW2Dye {
 		return BaseRGBValues;
 	}
 	
-	public HashMap<String, Long> getWhenAppliedToClothMap(){
+	public HashMap<String, Number> getWhenAppliedToClothMap(){
 		return Cloth;
 	}
 	
@@ -83,7 +111,7 @@ public class GW2Dye {
 		return clothRGB;
 	}
 	
-	public HashMap<String, Long> getWhenAppliedToLeatherMap(){
+	public HashMap<String, Number>getWhenAppliedToLeatherMap(){
 		return Leather;
 	}
 	
@@ -91,7 +119,7 @@ public class GW2Dye {
 		return leatherRGB;
 	}
 	
-	public HashMap<String, Long> getWhenAppliedToMetalMap(){
+	public HashMap<String, Number> getWhenAppliedToMetalMap(){
 		return Metal;
 	}
 	
